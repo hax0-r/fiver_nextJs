e277ef64e5e49d81042d3ed5306b27d624402a90
Muhammad <talha18513@gmail.com>
2024-12-27 21:30:29 +0500

    phonr add

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ export default function Home() {
       <div className="flex items-center justify-center bg-[#000000]">
         <div className="max-w-7xl w-full mx-auto p-4">
           <div className="flex items-center gap-6">
-            <p className="text-white">Phone: 07071354784 </p>
+            <p className="text-white">Phone: 07071354784 z </p>
           </div>
         </div>
       </div>
